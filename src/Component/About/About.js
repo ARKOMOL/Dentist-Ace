3bda61b0223a7ae8afa708363c000af932627cfd
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-04-18 00:57:52 +0600

    about part done

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-           <h1>this is about part </h1> 
+        <div className='container mx-auto px-10'>
+           <h1 className=' text-4xl text-center pt-7 pb-2 '>
+            Abdur Rashid
+           </h1>
+           <h3 className='text-2xl'>
+            My goal is to be a firstly a Jr. Web Developer and then a Sr. Web Developer.I want to do internship with a company beacuse if i do intern in a organaization or a company i can work with some developer and  gather some experience with a group that will help me in my goal. <br /> <br />
+            I am doing hard working to achieve my because without it i can't reach to my goal.Now i am doing a web development course.And i am spending time with this everyday 5 to 7 hours.It helps me perfectly complete my regular task and milestone what i make for me to increase my skill.In this time i findout my problems and try to slove it,in this case i take help from the google and other resources these can help me more.
+           </h3>
         </div>
     );
 };
